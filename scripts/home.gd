3459dcde038a4e3b0487b699acdcc740b8943e83
Saarth Rajan <149202885+SaarthRajan@users.noreply.github.com>
2025-10-03 16:21:17 -0400

    Made Placeholder Home Screen

---
+++ scripts/home.gd
@@ -0,0 +1,12 @@
+extends Control
+
+# Replace Placeholder Name with Title
+# Change BG Image
+# Change Font Style
+
+func _on_play_button_up() -> void:
+	get_tree().change_scene_to_file("res://scenes/hallway.tscn")
+
+
+func _on_quit_button_up() -> void:
+	get_tree().quit()
