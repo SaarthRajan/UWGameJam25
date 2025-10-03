fb5468e4a3fd5dc718a49fa7b64b0ec96b23f2ee
Saarth Rajan <149202885+SaarthRajan@users.noreply.github.com>
2025-10-03 19:18:42 -0400

    Fixed controls for back/forth

--- scripts/player_hallway.gd
+++ scripts/player_hallway.gd
@@ -1,15 +1,14 @@
 extends CharacterBody3D
 
+# Constants
+const SPEED = 50.0
 
-const SPEED = 25.0
-const JUMP_VELOCITY = -25.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Variables
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
@@ -22,8 +21,11 @@ func _physics_process(delta):
 	var move = Input.get_axis("Move_Down", "Move_Up")
 
 	if move:
-		velocity.x = move * SPEED
+		var forward_dir = transform.basis.x
+		velocity.x = forward_dir.x * move * SPEED
+		velocity.z = forward_dir.z * move * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
