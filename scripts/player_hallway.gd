420b63102d34a3e7f46202c04f6e8f2da05a6b6b
Saarth Rajan <149202885+SaarthRajan@users.noreply.github.com>
2025-10-03 19:25:26 -0400

    some teeny-tiny stuf

--- scripts/player_hallway.gd
+++ scripts/player_hallway.gd
@@ -6,25 +6,26 @@ const SPEED = 50.0
 # Variables
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
+	
 	# Gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
+	# Rotation
 	var input_dir = Input.get_axis("Move_Left", "Move_Right")
 	if input_dir:
 		rotate_y(delta * -input_dir)
-	else:
-		rotate_y(0)
 	
+	# Movement
 	var move = Input.get_axis("Move_Down", "Move_Up")
-
 	if move:
+		# Accelerate
 		var forward_dir = transform.basis.x
 		velocity.x = forward_dir.x * move * SPEED
 		velocity.z = forward_dir.z * move * SPEED
 	else:
+		# Deccelerate
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
