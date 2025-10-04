196a92bfab4eb486a2437e37c6dfe7bb93aae431
sadbunnygit <danelle06@outlook.com>
2025-10-04 17:16:29 -0400

    more scenes

--- scripts/home.gd
+++ scripts/home.gd
@@ -5,8 +5,8 @@ extends Control
 # Change Font Style
 
 func _on_play_button_up() -> void:
+	Dialogic.start("res://scripts/timelines/scene_3/room102.dtl")
 	get_tree().change_scene_to_file("res://scenes/hallway.tscn")
-
 
 func _on_quit_button_up() -> void:
 	get_tree().quit()
