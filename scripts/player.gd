013b5ceeedc48e59e96dbf5558e7fe6a832fb36c
Luna Xin <luna.xin@outlook.com>
2025-10-05 00:38:38 -0400

    add new anon sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,8 +14,19 @@ func get_input():
 func _physics_process(delta):
 	var playerInput = get_input()
 	if playerInput.is_zero_approx():
-		velocity = Vector2.ZERO
+		velocity = Vector2.ZERO 
+		_animation_player.play("idle_forward")
 	else:
 		velocity = playerInput * speed
+		if playerInput.x > 0:
+			_animation_player.play("walk_right")
+			_animation_player.flip_h = false
+		elif playerInput.x < 0:
+			_animation_player.play("walk_left")
+			_animation_player.flip_h = true
+		elif playerInput.y > 0:
+			_animation_player.play("walk_forward")
+		else:
+			_animation_player.play("walk_backwards")
 	
 	move_and_slide()
