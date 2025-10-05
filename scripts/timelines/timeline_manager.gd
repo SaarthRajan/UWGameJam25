662b61b4742feba1df4c0cbe4f77346da830b133
cindyli28.mail@gmail.com <cindyli28.mail@gmail.com>
2025-10-05 13:27:05 -0400

    const

--- scripts/timelines/timeline_manager.gd
+++ scripts/timelines/timeline_manager.gd
@@ -1,12 +1,10 @@
 extends Node
 
 # ######## CONSTS
-var scene_room_100: String = "res://scenes/cafeteria.tscn"
-var scene_room_101: String = "res://scenes/room-101.tscn"
-var scene_room_102: String = "res://scenes/room-102.tscn"
-var scene_room_103: String = "res://scenes/room-103.tscn"
-var scene_room_104: String = "res://scenes/room-104.tscn"
+const SCENE_ROOM_100: String = "res://scenes/cafeteria.tscn"
+const SCENE_ROOM_101: String = "res://scenes/room-101.tscn"
+const SCENE_ROOM_102: String = "res://scenes/room-102.tscn"
+const SCENE_ROOM_103: String = "res://scenes/room-103.tscn"
+const SCENE_ROOM_104: String = "res://scenes/room-104.tscn"
 
-var tl_3_fdr: String = "res://scripts/timelines/scene_3"
-
-var scene_3_room100 = SceneNode
+const TL_3_FDR: String = "res://scripts/timelines/scene_3"
