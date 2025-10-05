ad74e23d4814bb1ec379230d8ffa74dee076329e
cindyli28.mail@gmail.com <cindyli28.mail@gmail.com>
2025-10-05 13:09:24 -0400

    add spawnpoint

--- scripts/scene_node.gd
+++ scripts/scene_node.gd
@@ -1,10 +1,13 @@
 class_name SceneNode extends Node2D
 
 var _target_scene: String
+var _spawn_point: String
 var _tl_path: String
 var _next : SceneNode 
 
-func _init(target_scene, tl_path, next):
+func _init(target_scene, spawn_point, tl_path, next):
 	_target_scene = target_scene
+	_spawn_point = spawn_point
 	_tl_path = tl_path
 	_next = next
+	
