47e83d293a08dcf274718820ab2ff4b3f3c3c464
cindyli28.mail@gmail.com <cindyli28.mail@gmail.com>
2025-10-05 14:20:12 -0400

    add hallway scene

--- scripts/timelines/timeline_manager.gd
+++ scripts/timelines/timeline_manager.gd
@@ -6,6 +6,7 @@ const SCENE_ROOM_101: String = "res://scenes/room-101.tscn"
 const SCENE_ROOM_102: String = "res://scenes/room-102.tscn"
 const SCENE_ROOM_103: String = "res://scenes/room-103.tscn"
 const SCENE_ROOM_104: String = "res://scenes/room-104.tscn"
+const SCENE_HALLWAY: String = "res://scenes/hallway.tscn"
 
 const TL_3_FDR: String = "res://scripts/timelines/scene_3/"
 const TL_8_FDR: String = "res://scripts/timelines/date_murders/"
